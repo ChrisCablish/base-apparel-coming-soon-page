ba7653b1f5fc6f7b32da8bef8b89449a67d052b0
Chris <chriscablish@hotmail.com>
2023-03-07 09:31:51 -0600

    finalize scripting and postioning

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.getElementById('input');
 const submitButton = document.getElementById('submit');
 const form = document.getElementById('form');
+const alertIcon = document.getElementById('alert__icon');
+const alertMessage = document.getElementById('alert__message');
 
 const validateEmail = (input) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -12,7 +14,11 @@ form.addEventListener('submit', (e)=>{
     const email = input.value;
     e.preventDefault();
     if (!validateEmail(email)) {
-        alert('invalid');
+        alertIcon.style.display = 'block';
+        alertMessage.style.opacity = '1';
+        input.value = "";
+
+        // alert('invalid');
     } else {
         window.location.reload();
     }
